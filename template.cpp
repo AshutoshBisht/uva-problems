409d8087621252d7933179cbe4bb3d090979ac11
Ashutosh Bisht <bishtashutosh0@gmail.com>
2021-06-07 22:43:53 +0530

    initial commit

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define DEBUG
+
+int main()
+{
+#ifdef DEBUG
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    auto bkp_in = cin.rdbuf();
+    auto bkp_out = cout.rdbuf();
+    cin.rdbuf(in.rdbuf());
+    cout.rdbuf(out.rdbuf());
+#endif
+    /* #################### CODE START ################## */
+
+    /* #################### CODE END ################### */
+#ifdef DEBUG
+    cin.rdbuf(bkp_in);
+    cout.rdbuf(bkp_out);
+#endif
+}
